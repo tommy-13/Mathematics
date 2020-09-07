0a412f6929ae45231a3d68ca3b0af3df8227dab3
tommy-13 <tommy.the.snail@gmail.com>
2020-09-07 10:33:28 +0200

    Update DrawingTest.java

--- src/mathsol/functions/draw/test/DrawingTest.java
+++ src/mathsol/functions/draw/test/DrawingTest.java
@@ -5,8 +5,10 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import mathsol.functions.basic.GaussianDensityFunction;
 import mathsol.functions.basic.LinearFunction;
 import mathsol.functions.draw.ContinousFunctionPanel;
+import mathsol.random.variables.continous.GaussianContinousRandomVariable;
 
 public class DrawingTest {
 
@@ -18,8 +20,9 @@ public class DrawingTest {
 		//frame.add(new JLabel("Funktion zeichnen"), BorderLayout.NORTH);
 
 		//JPanel panel = new ContinousFunctionPanel(new LinearFunction(0, 1), -20, 20);
-		JPanel panel = new ContinousFunctionPanel(new LinearFunction(0, 1), -200, -100, 1200, 800);
+		//JPanel panel = new ContinousFunctionPanel(new LinearFunction(0, 1), -200, -100, 1200, 800);
 		//JPanel panel = new ContinousFunctionPanel(new QuadraticFunction(1, -2, -1), -5, 5, 1000, 800);
+		JPanel panel = new ContinousFunctionPanel(new GaussianDensityFunction(0, 4), -5, 5, 800, 800);
 		frame.add(panel, BorderLayout.CENTER);
 		
 		
