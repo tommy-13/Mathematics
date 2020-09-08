1310e9baffa29520d27501a9183f14038cb8d75a
tommy-13 <tommy.the.snail@gmail.com>
2020-09-08 10:09:28 +0200

    Update TrapezeRuleIntegralCalculator.java

--- src/mathsol/functions/integral/TrapezeRuleIntegralCalculator.java
+++ src/mathsol/functions/integral/TrapezeRuleIntegralCalculator.java
@@ -19,14 +19,16 @@ public class TrapezeRuleIntegralCalculator extends IntegralCalculator {
 		do {
 			sum      = 0;
 			checkSum = 0;
+
+			double gap      = (upperX - lowerX) / sectors;
 			
 			for(int s=0; s<sectors; s++) {
-				double gap      = (upperX - lowerX) / sectors;
 				double lowerVal = lowerX + s * gap;
 				double fLow     = function.getYValue(lowerVal);
 				double fHigh    = function.getYValue(lowerVal + gap);
 				double fMiddle  = function.getYValue(lowerVal + gap/2);
 				
+				// use simpson rule to approximate accuracy
 				sum      += gap * (fLow + fHigh) / 2;
 				checkSum += gap * (fLow + 4 * fMiddle + fHigh) / 6;
 			}
@@ -34,6 +36,7 @@ public class TrapezeRuleIntegralCalculator extends IntegralCalculator {
 			sectors *= 2;
 		} while (Math.abs(sum - checkSum) > tolerance && sectors < Integer.MAX_VALUE / 2);
 		
+		// return result of simpson rule, which should be more accurate
 		return checkSum;
 	}
 
