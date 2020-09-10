2278f9065dd258f884862b0b39264e9fe0cb7358
tommy-13 <tommy.the.snail@gmail.com>
2020-09-10 16:05:22 +0200

    Added a calculator for roots of a continous function

---
+++ src/mathsol/functions/root/RootCalculator.java
@@ -0,0 +1,68 @@
+package mathsol.functions.root;
+
+import mathsol.functions.basic.DoubleFunction;
+
+public abstract class RootCalculator {
+	
+	protected DoubleFunction f;
+	protected double         start = 0.0;
+	protected double         tolerance = 1e-5;
+	
+	
+	/**
+	 * Creates a new RootCalculator for the function <code>f</code>.
+	 * @param f function
+	 */
+	public RootCalculator(DoubleFunction f, double startingPoint) {
+		this.f = f;
+		this.start = startingPoint;
+	}
+
+	/**
+	 * Calculates a root of <code>f</code>, that is, a value x such that f(x)=0
+	 * @param f function
+	 * @return a root of <code>f</code>
+	 */
+	public abstract double getRoot();
+	
+	/**
+	 * Sets a new starting point.
+	 * @param startingPoint new starting point
+	 */
+	public void setStartingPoint(double startingPoint) {
+		this.start = startingPoint;
+	}
+	/**
+	 * Returns the starting point.
+	 * @return starting point
+	 */
+	public double getStartingPoint() {
+		return start;
+	}
+	
+	/**
+	 * Returns the function.
+	 * @return function
+	 */
+	public DoubleFunction getFunction() {
+		return f;
+	}
+	
+	/**
+	 * Returns the tolerance for the root.
+	 * @return tolerance
+	 */
+	public double getTolerance() {
+		return tolerance;
+	}
+	/**
+	 * Sets the tolerance for the root.
+	 * @param tolerance
+	 */
+	public void setTolerance(double tolerance) {
+		if (tolerance < 0) {
+			throw new IllegalArgumentException("Tolerance must be a positiv number");
+		}
+		this.tolerance = tolerance;
+	}
+}
